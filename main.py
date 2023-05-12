9e8ffd33d1323bab665781cb9a0635f137c1269e
Daniel Rosel <daniel@alves.world>
2023-05-12 17:49:10 +0200

    comments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 import requests
 import gi
-gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+gi.require_version("Gtk", "3.0") # Make sure we have Gtk 3.0
+from gi.repository import Gtk # Import Gtk 3.0
 
 from windows import GreetingWindow
 
@@ -10,8 +10,10 @@ def main():
     # Create a new window to greet the user
     win = GreetingWindow()
     win.connect("destroy", Gtk.main_quit)
+    # Display the window
     win.show_all()
+    # Start the Gtk main loop
     Gtk.main()
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": # If this file is run directly
+    main() # Run the program
