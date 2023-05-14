df98c90a85693d05c6b6a22b3731c8e0dabfd9f8
Daniel Rosel <daniel@alves.world>
2023-05-14 15:36:02 +0200

    rm dist

--- dist/main/tcl/graphviz/pkgIndex.tcl
+++
@@ -1,9 +0,0 @@
-package ifneeded Tcldot 8.0.5 "
-	load [file join $dir libtcldot.so] Tcldot"
-package ifneeded Tclpathplan 8.0.5 "
-	load [file join $dir libtclplan.so] Tclpathplan"
-package ifneeded Gdtclft 8.0.5 "
-	load [file join $dir libgdtclft.so] Gdtclft"
-package ifneeded gv 0 "
-	load [file join $dir libgv_tcl.so] gv"
-# end
